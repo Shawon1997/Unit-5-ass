4996ea987ca2e33273a791e4cb3267c7055a1ee7
Shawon1997 <16haw87@gmail.com>
2022-04-07 23:01:36 +0530

    final

--- webpack/build/bundle.js
+++ webpack/build/bundle.js
@@ -1,1 +1,32 @@
-(()=>{const e=document.getElementById("root"),n=document.createElement("h1");n.innerHTML="my name is shawon biswas, right now i am in masai",e.append(n)})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("//const path = require(\"path\")\r\n\r\nconst root = document.getElementById(\"root\")\r\nconst h1 = document.createElement(\"h1\")\r\nconst image = document.createElement(\"img\")\r\nimage.src = \"https://img.xcitefun.net/users/2015/01/382650,xcitefun-philippines-tour-1.jpg\";\r\n\r\n//enter your text\r\n\r\n\r\nh1.innerHTML = \"my name is shawon biswas, right now i am in masai\"\r\n\r\nroot.append(h1, image)\n\n//# sourceURL=webpack://react_ass/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,9 +2,12 @@
 
 const root = document.getElementById("root")
 const h1 = document.createElement("h1")
-    //enter your text
+const image = document.createElement("img")
+image.src = "https://img.xcitefun.net/users/2015/01/382650,xcitefun-philippines-tour-1.jpg";
+
+//enter your text
 
 
 h1.innerHTML = "my name is shawon biswas, right now i am in masai"
 
-root.append(h1)
+root.append(h1, image)
