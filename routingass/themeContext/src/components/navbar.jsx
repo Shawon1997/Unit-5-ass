eaff7b129aa3265d6e604783b37354d8d27ab546
Shawon1997 <16haw87@gmail.com>
2022-04-24 12:39:12 +0530

    updated

--- routingass/themeContext/src/components/navbar.jsx
+++ routingass/themeContext/src/components/navbar.jsx
@@ -3,8 +3,8 @@ import {useContext} from "react"
 export const Navbar=()=>{
     const {istheme,handlechange}=useContext(ThemeContext)
     //const {theme,handlechange}=useContext(ThemeContext)
-    return <div style={{background:{istheme?"black":"red"}}}>
-        <button onClick={()=>{
+    return <div style={{background:istheme?"black":"red",height:"1000px"}}>
+        <button style={{color:istheme?"red":"black"}} onClick={()=>{
             handlechange(true)
             //console.log(istheme)
         }}>Change theme</button>
